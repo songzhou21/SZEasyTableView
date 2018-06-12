95dfde19547eddd43c99db3f430202984898d2c6
Song Zhou <zhousong1993@gmail.com>
2018-06-12 17:32:03 +0800

    Initial Commit

---
+++ SZEasyTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZEasyTableView
+//
+//  Created by songzhou on 2018/6/12.
+//  Copyright © 2018年 songzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
